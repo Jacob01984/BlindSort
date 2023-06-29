6031e0f79beaf42b0a30668da0a44891d2e78ee9
Jacob01984 <Jacoblavenant@yahoo.com>
2023-06-29 14:18:59 -0500

    Initial Commit

---
+++ RandomNumberList/RandomNumberListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberListApp.swift
+//  RandomNumberList
+//
+//  Created by Jacob Lavenant on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
